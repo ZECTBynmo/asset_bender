572eaf2e3b88cd9c84c66f8abbe977169ebfbeec
Tim Finley <tfinley@hubspot.com>
2013-02-14 22:54:32 -0500

    Reorganized so that the error definition happens before most requires.

--- lib/asset_bender.rb
+++ lib/asset_bender.rb
@@ -3,19 +3,17 @@ require "asset_bender/patches/transform_hash_keys_to_symbols"
 
 require "custom_singleton"
 
-require "asset_bender/version_utils"
-require "asset_bender/conf_utils"
-require "asset_bender/logger_utils"
-require "asset_bender/http_utils"
-require "asset_bender/proc_utils"
-
 
 module AssetBender
   VERSION = "0.1.0"
 
-  extend LoggerUtils
-
   class Error < StandardError; end
+
+  require "asset_bender/version_utils"
+  require "asset_bender/conf_utils"
+  require "asset_bender/logger_utils"
+  require "asset_bender/http_utils"
+  require "asset_bender/proc_utils"
 
   autoload :Project,           "asset_bender/project"
   autoload :FilesystemProject, "asset_bender/filesystem_project"
